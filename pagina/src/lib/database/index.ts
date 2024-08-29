68aef7f9f0ab8243dade1dc2584515a68f2d35ea
steven <steven.cifuentesor@lola.edu.co>
2024-08-29 14:24:46 -0500

    cambios para el correcto deploy de vercel

--- pagina/src/lib/database/index.ts
+++ pagina/src/lib/database/index.ts
@@ -1,11 +1,11 @@
-import {TURSO_DATABASE_URL, TURSO_AUTH_TOKEN} from "$env/static/private";
+import {TURSO_CONNECTION_URL, TURSO_AUTH_TOKEN} from "$env/static/private";
 import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
 
 const client = createClient({
-  url: TURSO_DATABASE_URL!,
+  url: TURSO_CONNECTION_URL!,
   authToken: TURSO_AUTH_TOKEN!,
 });
 
